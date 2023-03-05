6829da08676c497e804e0bc4724aedcea998da9a
Lucas Walter <wsacul@gmail.com>
2023-03-05 12:55:01 -0800

    cleanup and making this work with python3 & ubuntu 22.10

--- test/wall_detector_test.py
+++ test/wall_detector_test.py
@@ -1,14 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
-import sys, unittest, time
+import sys
+import time
+import unittest
+
 import numpy as np
-import rospy, rostest
+import rospy
+import rostest
 from rostest_example.msg import Line, ScanPoints
+
 
 class TestWallDetector(unittest.TestCase):
     def __init__(self, *args):
         super(TestWallDetector, self).__init__(*args)
-    
+
     def callback(self, data):
         self.line_params = [data.m, data.b] # Store to check values in test cases
         self.done = True # Set done to true in order to end waiting loop
@@ -16,10 +21,10 @@ class TestWallDetector(unittest.TestCase):
     # First test case
     def test_wall_detector_1(self):
         self.done = False
-        
+
         # Create test node which will be publishing and subscribing to topics that wall_detector interfaces with.
         rospy.init_node("wall_detector_test")
-        pub = rospy.Publisher("points", ScanPoints)
+        pub = rospy.Publisher("points", ScanPoints, queue_size=3)
 
         gt_m, gt_b = 1, 10 # The ground truth slope and intercept of the line that WallDetector should find.
         # Generate data points. Can also add random noise here to test for robustness.
@@ -32,13 +37,13 @@ class TestWallDetector(unittest.TestCase):
         points.y = y.tolist()
 
         sub = rospy.Subscriber("line", Line, self.callback)
-        
+
         # Wait ten seconds -- can use ROS blocking functions instead
         timeout_t = time.time() + 10.0 #10 seconds
         while not rospy.is_shutdown() and not self.done and time.time() < timeout_t:
             pub.publish(points)
             time.sleep(0.1)
-        
+
         self.assertAlmostEqual(gt_m, self.line_params[0])
         self.assertAlmostEqual(gt_b, self.line_params[1])
 
@@ -47,7 +52,7 @@ class TestWallDetector(unittest.TestCase):
         self.done = False
 
         rospy.init_node("wall_detector_test")
-        pub = rospy.Publisher("points", ScanPoints)
+        pub = rospy.Publisher("points", ScanPoints, queue_size=3)
 
         gt_m, gt_b = -3, -1 # The ground truth slope and intercept of the line that WallDetector should find.
         x = np.array([1, 2, 3, 4, 5, 6])
@@ -59,13 +64,13 @@ class TestWallDetector(unittest.TestCase):
         points.y = y.tolist()
 
         sub = rospy.Subscriber("line", Line, self.callback)
-        
+
         # Wait ten seconds -- can use ROS blocking functions instead
         timeout_t = time.time() + 10.0 #10 seconds
         while not rospy.is_shutdown() and not self.done and time.time() < timeout_t:
             pub.publish(points)
             time.sleep(0.1)
-        
+
         self.assertAlmostEqual(gt_m, self.line_params[0])
         self.assertAlmostEqual(gt_b, self.line_params[1])
 
